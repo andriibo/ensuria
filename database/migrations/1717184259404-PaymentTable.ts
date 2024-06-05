a0b6c3583b5ca4f1ef724cc3df6857c336b3cadb
andrii <bondarenkoa1990@gmail.com>
2024-06-05 18:18:01 +0300

    status enums

--- database/migrations/1717184259404-PaymentTable.ts
+++ database/migrations/1717184259404-PaymentTable.ts
@@ -4,6 +4,7 @@ export class PaymentTable1717184259404 implements MigrationInterface {
     name = 'PaymentTable1717184259404'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TYPE "public"."payment_status_enum" AS ENUM('new', 'proceed', 'done', 'paid')`);
         await queryRunner.query(`CREATE TABLE "payment" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "shop_id" uuid NOT NULL, "amount" numeric(10,2) NOT NULL, "status" "public"."payment_status_enum" NOT NULL DEFAULT 'new', "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_fcaec7df5adf9cac408c686b2ab" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "payment" ADD CONSTRAINT "FK_f0662f97638fcfe621e3c371fa9" FOREIGN KEY ("shop_id") REFERENCES "shop"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
     }
@@ -11,6 +12,7 @@ export class PaymentTable1717184259404 implements MigrationInterface {
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "payment" DROP CONSTRAINT "FK_f0662f97638fcfe621e3c371fa9"`);
         await queryRunner.query(`DROP TABLE "payment"`);
+        await queryRunner.query(`DROP TYPE "public"."payment_status_enum"`);
     }
 
 }
